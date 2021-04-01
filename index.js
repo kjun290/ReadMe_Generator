cba553b6832f38c7c32d5144a04efec5b4ede949
Kevin Jun <kwjun90@gmail.com>
2021-04-01 15:11:25 -0500

    add 2 questions and makde writetofile function

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 // TODO: Include packages needed for this application
-const inquirer
+const inquirer = require("inquirer")
 const fs = require("fs")
+const path = require("path")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile("test.md", '#username')
+    inquirer.prompt(questions).then(responses => {
+        console.log(responses)
+    })
+}
 
 // Function call to initialize app
 init();
